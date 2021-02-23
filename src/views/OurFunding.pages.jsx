172f58cef0871b83c9f5e9aa5e1b2b7949524fa3
Charles <charlesiddle99@gmail.com>
2021-02-23 23:27:13 +0300

    added the our-funding page components

--- src/views/OurFunding.pages.jsx
+++ src/views/OurFunding.pages.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+import Parallax from "../components/Parallax";
+import cover from "../assets/cover.jpg";
+import styles from "../assets/landingPage.js";
+import { makeStyles } from "@material-ui/core/styles";
+import GridContainer from "../components/GridContainer";
+import GridItem from "../components/GridItem";
+import Partners from "../components/our-funding/Partners.component";
+
+const useStyles = makeStyles(styles);
 
 function OurFunding() {
+  const classes = useStyles();
   return (
     <div>
       {/* Funding
             Patnerships */}
-      This is the funding page
+      <Parallax filter image={cover}>
+        <div className={classes.container}>
+          <GridItem>
+            <GridItem xs={12} sm={12} md={6}>
+              <h1 className={classes.title}>Thinking of contributing</h1>
+              <h4>
+                A little help now can contribute to someone else's future.
+              </h4>
+              <br />
+              {/* <Button
+                color="danger"
+                size="lg"
+                href="/our-portfolio"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-play" />
+                Checkout our portfolio
+              </Button> */}
+            </GridItem>
+            <Partners />
+          </GridItem>
+        </div>
+      </Parallax>
     </div>
   );
 }
