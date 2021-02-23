1c58d12598ad58ddfee51cfdec0e56e0b42e707c
Charles <charlesiddle99@gmail.com>
2021-02-23 23:25:57 +0300

    added the engage page components

--- src/views/Engage.pages.jsx
+++ src/views/Engage.pages.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import ContactComponent from "../components/engage/Contact.component";
+import EngageHeaderComponent from "../components/engage/EngageHeader.component";
+import SnackBarComponent from "../components/engage/SnackBar.component";
+import Testimonial from "../components/engage/Testimonial.component";
+import SectionCarousel from "../components/engage/SectionCarousel.js";
 
 function Engage() {
   return (
     <div>
-      {/* Donate/sponsor
-            Blog
-            Contact us
-             */}
-      This is the engage page
+      <EngageHeaderComponent />
+      <ContactComponent />
+      <Testimonial />
     </div>
   );
 }
