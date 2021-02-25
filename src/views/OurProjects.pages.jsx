ba947a6492a261668305f61ee905009c8562afcd
Charles <charlesiddle99@gmail.com>
2021-02-25 23:47:51 +0300

    sdded ourProjects components

--- src/views/OurProjects.pages.jsx
+++ src/views/OurProjects.pages.jsx
@@ -1,7 +1,177 @@
 import React from "react";
+import GridContainer from "../components/GridContainer";
+import GridItem from "../components/GridItem";
+import NavPills from "../components/our-projects/NavPills";
 
 function OurProjects() {
-  return <div>Our projects go here</div>;
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={8} lg={6}>
+        <NavPills
+          color="info"
+          tabs={[
+            {
+              tabButton: "Human rights",
+              // tabIcon: Dashboard,
+              tabContent: (
+                <span>
+                  <p>
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                </span>
+              ),
+            },
+            {
+              tabButton: "Education",
+              // tabIcon: Schedule,
+              tabContent: (
+                <span>
+                  <p>
+                    Efficiently unleash cross-media information without
+                    cross-media value. Quickly maximize timely deliverables for
+                    real-time schemas.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically maintain clicks-and-mortar solutions without
+                    functional solutions. Dramatically visualize customer
+                    directed convergence without revolutionary ROI.
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                  </p>
+                </span>
+              ),
+            },
+            {
+              tabButton: "Girl child empowerment",
+              // tabIcon: List,
+              tabContent: (
+                <span>
+                  <p>
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                </span>
+              ),
+            },
+            {
+              tabButton: "Human rights violations",
+              // tabIcon: List,
+              tabContent: (
+                <span>
+                  <p>
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                </span>
+              ),
+            },
+            {
+              tabButton: "Child welfare",
+              // tabIcon: List,
+              tabContent: (
+                <span>
+                  <p>
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                </span>
+              ),
+            },
+            {
+              tabButton: "Athletics",
+              // tabIcon: List,
+              tabContent: (
+                <span>
+                  <p>
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                  <br />
+                  <p>
+                    Dramatically visualize customer directed convergence without
+                    revolutionary ROI. Collaboratively administrate empowered
+                    markets via plug-and-play networks. Dynamically
+                    procrastinate B2C users after installed base benefits.
+                  </p>
+                </span>
+              ),
+            },
+          ]}
+        />
+      </GridItem>
+    </GridContainer>
+  );
 }
 
 export default OurProjects;
